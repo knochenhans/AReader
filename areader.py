d34d28bd2e8fde766353b71159b05bfae5d95c94
Andre Jonas <nipsky@gmail.com>
2021-02-08 21:04:02 +0100

    initial commit (somewhat broken)

--- areader.py
+++ areader.py
@@ -1,12 +1,12 @@
+import tempfile
+import regex
+import json
+from shutil import copyfile
+from airium import Airium
+from gi.repository import Gtk, WebKit2
 import gi
 gi.require_version('Gtk', '3.0')
 gi.require_version('WebKit2', '4.0')
-from gi.repository import Gtk, WebKit2
-from airium import Airium
-from shutil import copyfile
-import json
-import regex
-import tempfile
 
 temp = tempfile.TemporaryFile()
 temp_dir = tempfile.mkdtemp()
@@ -34,93 +34,9 @@ def receiver(user_content_manager, javascript_result):
     window.on_load_uri(result["path"], result["line"])
 
 
-class FileChooserWindow(Gtk.Window):
-    def __init__(self):
-        Gtk.Window.__init__(self, title="FileChooser Example")
-
-        box = Gtk.Box(spacing=6)
-        self.add(box)
-
-        button1 = Gtk.Button(label="Choose File")
-        button1.connect("clicked", self.on_file_clicked)
-        box.add(button1)
-
-        button2 = Gtk.Button(label="Choose Folder")
-        button2.connect("clicked", self.on_folder_clicked)
-        box.add(button2)
-
-    def on_file_clicked(self, widget):
-        dialog = Gtk.FileChooserDialog(
-            title="Please choose a file", parent=self, action=Gtk.FileChooserAction.OPEN
-        )
-        dialog.add_buttons(
-            Gtk.STOCK_CANCEL,
-            Gtk.ResponseType.CANCEL,
-            Gtk.STOCK_OPEN,
-            Gtk.ResponseType.OK,
-        )
-
-        self.add_filters(dialog)
-
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            print("Open clicked")
-            print("File selected: " + dialog.get_filename())
-        elif response == Gtk.ResponseType.CANCEL:
-            print("Cancel clicked")
-
-        dialog.destroy()
-
-    def add_filters(self, dialog):
-        filter_text = Gtk.FileFilter()
-        filter_text.set_name("Text files")
-        filter_text.add_mime_type("text/plain")
-        dialog.add_filter(filter_text)
-
-        filter_py = Gtk.FileFilter()
-        filter_py.set_name("Python files")
-        filter_py.add_mime_type("text/x-python")
-        dialog.add_filter(filter_py)
-
-        filter_any = Gtk.FileFilter()
-        filter_any.set_name("Any files")
-        filter_any.add_pattern("*")
-        dialog.add_filter(filter_any)
-
-    def on_folder_clicked(self, widget):
-        dialog = Gtk.FileChooserDialog(
-            title="Please choose a folder",
-            parent=self,
-            action=Gtk.FileChooserAction.SELECT_FOLDER,
-        )
-        dialog.add_buttons(
-            Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL, "Select", Gtk.ResponseType.OK
-        )
-        dialog.set_default_size(800, 400)
-
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            print("Select clicked")
-            print("Folder selected: " + dialog.get_filename())
-        elif response == Gtk.ResponseType.CANCEL:
-            print("Cancel clicked")
-
-        dialog.destroy()
-
-
-class DialogWindow(Gtk.Window):
+class Window(Gtk.Window):
     def __init__(self, webview):
         Gtk.Window.__init__(self, title="Dialog Example")
-
-        # self.set_border_width(6)
-
-        #button = Gtk.Button(label="Open dialog")
-        #button.connect("clicked", self.on_button_clicked)
-
-        # self.add(button)
-
-        filechooser = FileChooserWindow()
-        filechooser.show_all()
 
         copyfile('style.css', temp_dir + '/style.css')
         copyfile('Topaz_a1200_v1.0.ttf', temp_dir + '/Topaz_a1200_v1.0.ttf')
@@ -140,7 +56,33 @@ class DialogWindow(Gtk.Window):
 
         content_manager.connect("script-message-received::signal", receiver)
 
-        with open('cyberblanker.guide', 'r', encoding='cp1252') as input_file:
+        # Load File
+
+        #filechooser = FileChooser()
+        #filechooser.connect("destroy", Gtk.main_quit)
+        # filechooser.show_all()
+
+        dialog = Gtk.FileChooserDialog(
+            "Please choose a file", self, Gtk.FileChooserAction.OPEN)
+        dialog.add_buttons(
+            Gtk.STOCK_CANCEL,
+            Gtk.ResponseType.CANCEL,
+            Gtk.STOCK_OPEN,
+            Gtk.ResponseType.OK,
+        )
+
+        filter_any = Gtk.FileFilter()
+        filter_any.set_name("AmigaGuide files")
+        filter_any.add_pattern("*.guide")
+        dialog.add_filter(filter_any)
+
+        filename = ""
+
+        if dialog.run() == Gtk.ResponseType.OK:
+            filename = dialog.get_filename()
+            dialog.destroy()
+
+        with open(filename, 'r', encoding='cp1252') as input_file:
             database = Database()
             nodes = []
 
@@ -264,6 +206,6 @@ class DialogWindow(Gtk.Window):
 
 
 webview = WebKit2.WebView()
-window = DialogWindow(webview)
+window = Window(webview)
 window.show_all()
 Gtk.main()
